279b109257c5ed1f85d0c9e2b7c7843fffadc90e
JeffeDev <jeffe.info@gmail.com>
2022-07-05 15:59:01 -0300

    Classes VO - CPF  e EMAIL

---
+++ src/main/java/br/com/pratica/rh/recursoshumanos/model/vo/Cpf.java
@@ -0,0 +1,13 @@
+package br.com.pratica.rh.recursoshumanos.model.vo;
+
+public class Cpf {
+	private String numero;
+
+	public Cpf(String numero) {
+	    if(numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")){
+	        throw new IllegalArgumentException("CPF invalido!");
+	    }
+		this.numero = numero;
+	}
+	
+}
---
+++ src/main/java/br/com/pratica/rh/recursoshumanos/model/vo/Email.java
@@ -0,0 +1,17 @@
+package br.com.pratica.rh.recursoshumanos.model.vo;
+
+public class Email {
+	private String endereco;
+
+	public Email(String endereco) {
+		if (endereco == null || !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")) {
+			throw new IllegalArgumentException("Email inválido!");
+		}
+		this.endereco = endereco;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+	
+}
